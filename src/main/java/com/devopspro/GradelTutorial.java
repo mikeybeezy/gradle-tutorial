9366e8a16c11a95c6946b0211347c7f84d51f192
Mike Babs <mikeybabs40@gmail.com>
2022-12-23 00:33:31 +0000

    Learning gradel and testing

---
+++ src/main/java/com/devopspro/GradelTutorial.java
@@ -0,0 +1,7 @@
+package com.devopspro;
+
+public class GradelTutorial {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/devopspro/GradelTutorialTest.java
@@ -0,0 +1,15 @@
+package com.devopspro;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class GradelTutorialTest {
+    @Test
+    public void verifyNoException() {
+        GradelTutorial.main(new String []{});
+    }
+}
+
+
